527f418e12fea65c31993d4c1296bf527890735c
Rafael Vargas <rafael.vargas@outlook.com>
2021-07-03 18:16:55 -0400

    update to nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
 console.log('hello world!')
+
+//
+
+function clickHandler(event) {
+  console.log(event.target())
+}
+
+// Add clickhandler to the page
+
+document.querySelector('body').addEventListener('click', clickHandler(event))
