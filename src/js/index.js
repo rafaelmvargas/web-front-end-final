0cc33b15d190703678435c28ba3fc78420e6fbd5
Rafael Vargas <rafael.vargas@outlook.com>
2021-07-04 02:15:44 -0400

    working nave with main having margin

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,16 @@ console.log('hello world!')
 function clickHandler(event) {
   console.log(event.target)
   event.preventDefault()
+
+  if (event.target.matches('#hamburger')) {
+    showMenu()
+    // event.preventDefault()
+  }
 }
 
+function showMenu() {
+  document.querySelector('body').classList.toggle('show-navigation-bar')
+}
 // Add clickhandler to the page
 
 document.addEventListener('click', clickHandler)
