5d7c671987cf881c0f178ea38e90679ffb049d1c
Rafael Vargas <rafael.vargas@outlook.com>
2021-07-06 16:56:01 -0400

    work on pokemon modal data get

--- src/js/modules/pokeDetails.mjs
+++ src/js/modules/pokeDetails.mjs
@@ -1,6 +1,22 @@
-function showPokeDetails(pokemon) {
+import { fetchPokemonData } from "./pokedex.mjs";
 
-    
+function showPokeDetails(pokemon) {
+  var pokemonModal = document.querySelector(".pokemon-modal");
+  let info = fetchPokemonData(pokemon.name);
+
+  Promise.all(info).then((values) => {
+    for (const [item, monster] of values.entries()) {
+      pokemonSummary.innerHTML += `<div class="pokemonCard" id="${monster.name}">
+      <img src="${monster.imageUrl}" id="${monster.name}">
+      <div class="monster-name"><a href="#" class="pokemon" id="${monster.name}">${monster.name}<div>
+      </a>
+       </div>`;
+    }
+
+    /**
+     * convert to unordered list
+     */
+  });
 }
 
 export { showPokeDetails };
